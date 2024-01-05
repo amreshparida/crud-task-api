eca3dc0c654349e2e93aacc162bc1e1048fcb86c
Amaresh Parida <amresh.parida10000@gmail.com>
2024-01-05 14:33:28 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # crud-task-api
 Django REST API to manage a Task entity with CRUD (Create, Read, Update, Delete) functionalities.
+
+
+
+API Endpoints
+List and Create Tasks
+URL: /api/tasks/
+Method: GET (List all tasks), POST (Create a new task)
+Retrieve, Update, and Delete a Specific Task
+URL: /api/tasks/<int:pk>/
+Method: GET (Retrieve), PUT (Update), DELETE (Delete)
+
+
+Project Structure
+task_manager/: Django project directory
+tasks/: Django app directory
+models.py: Task model definition
+serializers.py: Task model serializer
+views.py: Views for handling API endpoints
+urls.py: URL patterns for the app
+tests.py: Unit tests for the API
+
+Database: SQLite (configured in settings.py)
+
+
+## Setup
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/amreshparida/crud-task-api.git
+   cd crud-task-api
+   cd task_crud
+   pip install -r requirements.txt
+   python manage.py migrate
+   python manage.py runserver
+
+
+  
+
+
---
+++ task_crud/tasks/tests.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import Task
+
+class TaskAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.task_data = {'title': 'Test Task', 'description': 'Testing the task API', 'completed': False}
+        self.task = Task.objects.create(**self.task_data)
+        self.url = f'/api/v1/tasks/{self.task.id}/'
+
+    def test_get_all_tasks(self):
+        response = self.client.get('/api/v1/tasks/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_task(self):
+        response = self.client.post('/api/v1/tasks/', data=self.task_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_single_task(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_task(self):
+        updated_data = {'title': 'Updated Task', 'description': 'Updated description', 'completed': True}
+        response = self.client.put(self.url, data=updated_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_task(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
