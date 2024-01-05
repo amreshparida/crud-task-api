8e2efe206b2799a92939ce3a4d3cfbd6f16fb496
Amaresh Parida <amresh.parida10000@gmail.com>
2024-01-05 17:49:10 +0530

    fixed test with x-api-key
    fixed test with x-api-key

--- task_crud/tasks/tests.py
+++ task_crud/tasks/tests.py
@@ -9,29 +9,30 @@ class TaskAPITestCase(TestCase):
         self.task_data = {'title': 'Test Task', 'description': 'Testing the task API', 'completed': False}
         self.task = Task.objects.create(**self.task_data)
         self.url = f'/api/v1/tasks/{self.task.id}/'
+        self.api_key = 'secret-api-key'
 
     # get all tasks test
     def test_get_all_tasks(self):
-        response = self.client.get('/api/v1/tasks/')
+        response = self.client.get('/api/v1/tasks/', HTTP_X_API_KEY=self.api_key)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     #create tasks test
     def test_create_task(self):
-        response = self.client.post('/api/v1/tasks/', data=self.task_data)
+        response = self.client.post('/api/v1/tasks/', data=self.task_data,  HTTP_X_API_KEY=self.api_key)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     #get single task test
     def test_get_single_task(self):
-        response = self.client.get(self.url)
+        response = self.client.get(self.url,  HTTP_X_API_KEY=self.api_key)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     #update single test
     def test_update_task(self):
         updated_data = {'title': 'Updated Task', 'description': 'Updated description', 'completed': True}
-        response = self.client.put(self.url, data=updated_data)
+        response = self.client.put(self.url, data=updated_data, HTTP_X_API_KEY=self.api_key)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     #delete task test
     def test_delete_task(self):
-        response = self.client.delete(self.url)
+        response = self.client.delete(self.url, HTTP_X_API_KEY=self.api_key)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
